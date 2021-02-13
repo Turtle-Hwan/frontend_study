ac34232ab7230aac0e5f1693070c6fe267744615
김지환 <kjhwan0802@naver.com>
2021-02-13 17:57:39 +0900

    use classList.toggle

--- VanilaJS prac/practice/eventListener2/index.js
+++ VanilaJS prac/practice/eventListener2/index.js
@@ -3,12 +3,7 @@ const title = document.getElementById("title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const currentClass = title.className;
-    if (currentClass === CLICKED_CLASS) {
-        title.classList.remove(CLICKED_CLASS);
-    } else {
-        title.classList.add(CLICKED_CLASS);
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
