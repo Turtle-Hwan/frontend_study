f2f1fe14f500d9fbbc092328b249bae69b1db087
김지환 <kjhwan0802@naver.com>
2021-02-13 17:27:18 +0900

    className change to classList classLIst is more easy to use

--- VanilaJS prac/practice/eventListener2/index.js
+++ VanilaJS prac/practice/eventListener2/index.js
@@ -5,9 +5,9 @@ const CLICKED_CLASS = "clicked";
 function handleClick() {
     const currentClass = title.className;
     if (currentClass === CLICKED_CLASS) {
-        title.className = "";
+        title.classList.remove(CLICKED_CLASS);
     } else {
-        title.className = CLICKED_CLASS;
+        title.classList.add(CLICKED_CLASS);
     }
 }
 
